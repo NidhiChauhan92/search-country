cef42f67e1112bf7beda8220d14d5e8ed36f6433
Nidhi <ndhpanchal1034@gmail.com>
2025-03-25 21:49:15 +0530

    update

--- src/components/CountriesSearch.jsx
+++ src/components/CountriesSearch.jsx
@@ -1,29 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+// import axios from "axios";
 import Card from "./Card";
 
+
+
+
 function CountriesSearch() {
+
     const [countries, setCountries] = useState([]);
     const [search, setSearch] = useState("");
 
     const api = "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
 
+   
+
     useEffect(() => {
-        fetch(api)
-            .then((res) => res.json())
+        fetch(api) // API URL
+            .then((res) => res.json()) // Convert response to JSON
             .then((data) => {
-                console.log("Fetched Data:", data); // Debug API response
                 setCountries(data);
+                // console.log(data);
+
             })
             .catch((error) => console.error("Error fetching data: ", error));
     }, []);
 
-    // Filtering based on 'name.common'
+    // console.log(countries);
+
     const filteredCountries = countries.filter(country =>
         country.name?.common?.toLowerCase().includes(search.toLowerCase())
-    );
-
+      );
+      
+    //   console.log(filteredCountries);
     return (
         <>
+
             <header style={{
                 width: "100vw",
                 height: "50px",
@@ -32,32 +44,34 @@ function CountriesSearch() {
                 textAlign: "center",
                 justifyContent: "center",
                 alignItems: "center"
+
+
             }}>
                 <input style={{
                     width: "480px",
                     height: "20px"
-                }} type="text" 
-                    placeholder="Search for Countries..."
-                    value={search} 
-                    onChange={(e) => setSearch(e.target.value)} 
-                />
-            </header>
 
+                }} type="text" placeholder="Search for Contries.." value={search} onChange={(e) => setSearch(e.target.value)} />
+            </header>
             <div className="countryCard" style={{
                 display: "flex",
                 flexWrap: "wrap",
                 gap: "10px",
                 justifyContent: "center",
                 marginTop: "30px"
+
             }}>
-                {filteredCountries.length > 0 ? (
-                    filteredCountries.map((item, index) => (
-                        <Card key={index} flag={item.flags?.png} name={item.name?.common} />
-                    ))
-                ) : ""}
+
+                {filteredCountries.map((item) => (
+                    <Card flag={item.png} name={item.common} />
+                ))}
             </div>
         </>
-    );
+
+    )
+
+
+
 }
 
 export default CountriesSearch;
